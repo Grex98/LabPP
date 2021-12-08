60f2ce08220de8c92d6ccf84077866654b5b71a1
Grex <grex1999@mail.ru>
2021-12-08 17:21:46 +0300

    PP of elements

--- lab1.py
+++ lab1.py
@@ -1,32 +1,45 @@
 from multiprocessing import Pool
 
-def doubler(massiv):
-    print(massiv)
+
+def doubler(number):
+    #print(number)
     result = 1
-    for index in massiv:
-        #print(index)
-        element = [ int(c) for c in str(index)]
-        first = element[0]
-        second = element[1]
-        #print(first)
-        #print(second)
-        if first % 2 == second % 2:
-            result *= first
-            print(result)
-    return result
+    #for index in massiv:
+    #print(index)
+    element = [ int(c) for c in str(number)]
+    first = element[0]
+    second = element[1]
+    #print(first)
+    #print(second)
+    if first % 2 == second % 2:
+    #result = first
+    #print(first)
+        return first
+    else:
+        return 1
+
+
 
 
 
 
 if __name__ == '__main__':
-    numbers = [[165, 531, 884, 416],
+    matrix = [[165, 531, 884, 416],
     [494, 256, 779, 948],
     [398, 589, 832, 538],
     [416, 756, 432, 895]]
+    print('Original matrix: ' + str(matrix))
+    lst = []
+    for line in matrix:
+        lst += line
+    print('Matrix elements: ' + str(lst))
 
-    pool = Pool(processes=5)
-    result_process = pool.map(doubler, numbers)
-    r = 1
+    pool = Pool(processes=2)
+    result_process = (pool.map(doubler, lst))
+    result = 1
     for k in result_process:
-        r*=k
-    print(r)
+        result *= k
+    if result == 1:
+        print('Not foud valid numbers in matrix')
+    else:
+        print('Product of valid numbers: ' + str(result))
