e5020129430996f953628731831da44cc7418f84
Grex98 <grex1999@mail.ru>
2021-12-10 12:13:55 +0300

    Add potoks cecker

--- test.py
+++ test.py
@@ -6,25 +6,25 @@ def potok(lst):
     potoks = len(lst)//10
     return potoks
 
-def doubler(number):
+def parser(number):
     #print(number)
+    result = 1
     proc = os.getpid()
     #for index in massiv:
     #print(index)
     element = [ int(c) for c in str(number)]
     first = element[0]
     second = element[1]
+    print('{0} find to {1} by process id: {2}'.format(
+    number, first, proc))
     #print(first)
     #print(second)
     if first % 2 == second % 2:
-        #result = first
-        print('In {0} senior rank {1}. Process id: {2}'.format(number, first, proc))
+    #result = first
+        print(first)
         return first
     else:
-        print('{0} is not valid. Process id: {1}'.format(number, proc))
         return 1
-
-
 
 if __name__ == '__main__':
     n = randint(2, 10)
@@ -34,13 +34,16 @@ if __name__ == '__main__':
     for line in matrix:
         lst += line
     print('Matrix elements: ' + str(lst))
+    print (len(lst))
+    print(len(lst)//10)
+    print(potok(lst))
     current_potoks = potok(lst)
     pool = Pool(processes=current_potoks)
-    result_process = (pool.map(doubler, lst))
+    result_process = (pool.map(parser, lst))
     result = 1
-    for result_numbers in result_process:
-        result *= result_numbers
+    for k in result_process:
+        result *= k
     if result == 1:
-        print('Not found valid numbers in matrix')
+        print('Not foud valid numbers in matrix')
     else:
         print('Product of valid numbers: ' + str(result))
